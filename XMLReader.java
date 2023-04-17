87029d1f805d8d457205408097fce9fb46eda065
Brandon Filmer <brandonf2000@hotmail.com>
2023-04-17 13:41:57 +0200

    5. Now has SAX XML parser.

--- XMLReader.java
+++ XMLReader.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
 import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 public class XMLReader {
   public static void main(String[] args) {
@@ -17,48 +17,64 @@ public class XMLReader {
       System.out.print("Enter comma-separated field names to output: ");
       String input = scanner.nextLine();
       String[] fieldNames = input.split(",");
-      
+
       if (fieldNames.length == 0 || fieldNames[0].trim().equals("")) {
         throw new Exception("No fields entered");
       }
 
       File inputFile = new File("input.xml");
 
-      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      Document doc = dBuilder.parse(inputFile);
+      SAXParserFactory factory = SAXParserFactory.newInstance();
+      SAXParser saxParser = factory.newSAXParser();
 
-      doc.getDocumentElement().normalize();
+      DefaultHandler handler = new DefaultHandler() {
+        boolean fieldElement = false;
+        String currentFieldName = "";
+        String currentFieldValue = "";
 
-      NodeList nList = doc.getElementsByTagName("field");
+        List<JSONObject> fields = new ArrayList<>();
 
-      List<JSONObject> fields = new ArrayList<>();
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+          if (qName.equalsIgnoreCase("field")) {
+            fieldElement = true;
+            currentFieldName = attributes.getValue("name");
+            currentFieldValue = "";
+          }
+        }
 
-      for (int i = 0; i < nList.getLength(); i++) {
-        Node nNode = nList.item(i);
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+          if (qName.equalsIgnoreCase("field")) {
+            fieldElement = false;
 
-        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-          String name = nNode.getAttributes().getNamedItem("name").getTextContent();
-          String value = nNode.getTextContent();
-
-          for (String fieldName : fieldNames) {
-            if (name.equals(fieldName.trim())) {
-              JSONObject field = new JSONObject();
-              field.put("name", name);
-              field.put("value", value);
-              fields.add(field);
+            for (String fieldName : fieldNames) {
+              if (currentFieldName.equals(fieldName.trim())) {
+                JSONObject field = new JSONObject();
+                field.put("name", currentFieldName);
+                field.put("value", currentFieldValue.trim());
+                fields.add(field);
+              }
             }
           }
         }
-      }
 
-      if (fields.isEmpty()) {
-        System.out.println("No matching fields found");
-      } else {
-        JSONObject output = new JSONObject();
-        output.put("fields", fields);
-        System.out.println(output.toString());
-      }
+        public void characters(char ch[], int start, int length) throws SAXException {
+          if (fieldElement) {
+            currentFieldValue += new String(ch, start, length);
+          }
+        }
+
+        public void endDocument() throws SAXException {
+          if (fields.isEmpty()) {
+            System.out.println("No matching fields found");
+          } else {
+            JSONObject output = new JSONObject();
+            output.put("fields", fields);
+            System.out.println(output.toString());
+          }
+        }
+      };
+
+      saxParser.parse(inputFile, handler);
     } catch (Exception e) {
       e.printStackTrace();
     }
